2e18eb0aa39ba97b62cd5c26a1268cec72d67c8c
Saloni Pandey <salonipandey014@gmail.com>
2024-03-03 23:44:51 +0530

    fixed UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ const { image } = profile
   return (
     <>
       <header>
-        <nav className=" w-full flex justify-between md:p-5 p-2 gap-5 max-sm:flex-col border shadow-xl shadow-main">
+        <nav className=" w-full flex justify-between md:p-5 p-2 gap-5 max-sm:text-sm max-sm:items-center border shadow-xl shadow-main">
           <h1 className="mt-1 font-logo text-3xl font-extrabold text-heading">TaskFlow</h1>
-          <ul className="flex items-center  gap-5 font-body font-semibold  max-sm:top-20 max-sm:flex-col  max-sm:items-start text-heading text-lg ">
+          <ul className="flex items-center  gap-5 font-body font-semibold  max-sm:top-20   max-sm:items-start text-heading text-lg max-sm:mt-6">
             <NavLink to="/main/profile" className={({isActive})=>{
               console.log(isActive)
              return  isActive ? " text-heading font-bold px-2 py-1 border-b-2 border-heading": "text-heading text-lg"
