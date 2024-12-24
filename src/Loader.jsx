88b7ea8c5f3d96d0e5ea1f3593e4624e4df85a84
Saloni Pandey <123414664+Saloni-FED@users.noreply.github.com>
2024-12-24 16:56:45 +0530

    Update Loader.jsx

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ClipLoader } from "react-spinners";
 const Loader = () => {
     return (
-      <div className="flex justify-center  h-full">
+      <div className="flex justify-center  h-full mt-10">
         <ClipLoader color={"#6368d9"} size={50} />
       </div>
     );
